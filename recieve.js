e51e130bee56af5ec5ff2d4a0bd8b7bd5e17739c
Takuya Takahashi <info@yujilow.net>
2017-11-16 11:57:17 +0000

    rm log

--- recieve.js
+++ recieve.js
@@ -3,7 +3,7 @@ var Bleacon = require('bleacon');
 Bleacon.startScanning();
 var stopped;
 var on = false;
-var uuid = "cca4e3c135154dff9e80d5e1b0a402cf";
+var uuid = "b9407f30f5f8466eaff9255512200912";
 
 Bleacon.on('discover', function(bleacon) {
    if(bleacon.uuid !== uuid)return;
@@ -11,7 +11,6 @@ Bleacon.on('discover', function(bleacon) {
    stopped = setTimeout(farAway, 1000 * 60 * 5);
    if(!on){
      on = true;
-     console.log("on");
      light.on();
    }
 });
@@ -19,7 +18,6 @@ Bleacon.on('discover', function(bleacon) {
 function farAway(){
   if(on){
     on = false;
-    console.log("off");
     light.off();
   }
 }
